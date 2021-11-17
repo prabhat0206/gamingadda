5e86439b1e2b4f2d6a37a46df3203753a32b3d81
Prabhat Ranjan <Prabhat0206@hotmail.com>
2021-11-17 13:03:35 +0530

    new

--- src/connections/backend.ts
+++ src/connections/backend.ts
@@ -1,2 +1,2 @@
-export const backend_url = "https://virtual-fight-backend.vercel.app/";
-export const socket_url = "https://virtual-fight-backend.vercel.app/";
+export const backend_url = "https://virtual-fight.vercel.app/";
+export const socket_url = "https://virtual-fight.vercel.app/";
