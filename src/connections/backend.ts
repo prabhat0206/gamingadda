0032daf0335214b58745b833a0e924e5924df0dd
Prabhat Ranjan <Prabhat0206@hotmail.com>
2021-11-20 14:36:37 +0530

    new

--- src/connections/backend.ts
+++ src/connections/backend.ts
@@ -1,2 +1,2 @@
-export const backend_url = "https://virtual-fight.herokuapp.com/";
-// export const backend_url = "http://localhost:8000/";
+// export const backend_url = "https://virtual-fight.herokuapp.com/";
+export const backend_url = "http://localhost:8000/";
